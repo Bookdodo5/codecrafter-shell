bd78b153f08d961509ccaf0c9222a1254a2a625b
Bookdodo <bookdodo5@gmail.com>
2025-09-20 13:34:55 +0700

    #PN5 The exit builtin [2]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
             while(true) {
                 System.out.print("$ ");
                 String input = scanner.nextLine();
-                if(input.equals("exit")) {
+                if(input.equals("exit 0")) {
                     break;
                 }
                 System.out.println(input + ": command not found");
