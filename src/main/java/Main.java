678eb1fc34c07ea2110f477cc93f69534faa5ac2
Bookdodo <bookdodo5@gmail.com>
2025-09-20 13:59:16 +0700

    #MG5 The type builtin: executable files [1]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,60 @@
+
 import java.util.Scanner;
 import java.util.List;
+import java.io.File;
 
 public class Main {
+
+    private static String checkExecutable(String command, String[] path) {
+        String[] extensions = {"", ".exe", ".cmd", ".bat"};
+        for (String directory : path) {
+            for (String ext : extensions) {
+                File file = new File(directory, command + ext);
+                if (file.exists() && !file.isDirectory()) {
+                    return file.getAbsolutePath();
+                }
+            }
+        }
+        return null;
+    }
+
+    private static String type(String input) {
+        String[] parts = input.split("\\s+");
+        if (parts.length < 2) {
+            return "type: missing argument";
+        }
+        
+        String command = parts[1];
+        String PATH = System.getenv("PATH");
+        String[] path = PATH.split(File.pathSeparator);
+
+        List<String> builtins = List.of("echo", "type", "exit", "ls");
+
+        if (builtins.contains(command)) {
+            return command + " is a shell builtin";
+        }
+        
+        String executablePath = checkExecutable(command, path);
+        if (executablePath != null) {
+            return command + " is " + executablePath;
+        }
+        
+        return command + ": not found";
+    }
+
     public static void main(String[] args) throws Exception {
 
-        List<String> builtins = List.of("echo", "type", "exit");
-        
         try (Scanner scanner = new Scanner(System.in)) {
-            while(true) {
+            while (true) {
                 System.out.print("$ ");
                 String input = scanner.nextLine();
-                if(input.equals("exit 0")) {
+                if (input.equals("exit 0")) {
                     break;
-                }
-                else if(input.split(" ")[0].equals("echo")) {
+                } else if (input.split(" ")[0].equals("echo")) {
                     System.out.println(input.substring(input.indexOf(" ") + 1));
-                }
-                else if(input.split(" ")[0].equals("type")) {
-                    String command = input.split(" ")[1];
-                    if(builtins.contains(command)) {
-                        System.out.println(command + " is a shell builtin");
-                    }
-                    else {
-                        System.out.println(command + ": not found");
-                    }
-                }
-                else {
+                } else if (input.split(" ")[0].equals("type")) {
+                    System.out.println(type(input));
+                } else {
                     System.out.println(input + ": command not found");
                 }
             }
