18656c734c04c61e76a3ebe8d87055a945f69460
Bookdodo <bookdodo5@gmail.com>
2025-09-20 14:28:56 +0700

    #IP1 Run a program [5]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.util.List;
 import java.io.File;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
@@ -23,13 +24,39 @@ public class Main {
         if (builtins.contains(command)) {
             return command + " is a shell builtin";
         }
-        
+
         String executablePath = checkExecutable(command, path);
         if (executablePath != null) {
             return command + " is " + executablePath;
         }
-        
+
         return command + ": not found";
+    }
+
+    private static void run(String command, String argument) {
+        try {
+            String[] argumentParts = argument.split(" ");
+            String[] arguments = new String[argumentParts.length + 1];
+            arguments[0] = command;
+            System.arraycopy(argumentParts, 0, arguments, 1, argumentParts.length);
+            ProcessBuilder processBuilder = new ProcessBuilder(arguments);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error executing command: " + e.getMessage());
+            if (e instanceof InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -51,24 +78,8 @@ public class Main {
                 } else if (command.equals("type")) {
                     System.out.println(type(argument, path));
                 } else if (checkExecutable(command, path) != null) {
-                    String[] argumentParts = argument.split(" ");
-                    String[] arguments = new String[argumentParts.length + 1];
-                    arguments[0] = command;
-                    System.arraycopy(argumentParts, 0, arguments, 1, argumentParts.length);
-                    ProcessBuilder processBuilder = new ProcessBuilder(arguments);
-                    processBuilder.redirectErrorStream(true);
-                    Process process = processBuilder.start();
-                    
-                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            System.out.println(line);
-                        }
-                    }
-                    
-                    process.waitFor();
-                }
-                else {
+                    run(command, argument);
+                } else {
                     System.out.println(input + ": command not found");
                 }
             }
