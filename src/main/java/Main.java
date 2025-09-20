ad642b48abbb8a7d8b5df132e4c915d456954c68
Bookdodo <bookdodo5@gmail.com>
2025-09-20 13:42:17 +0700

    #EZ5 The type builtin: builtins [1]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        List<String> builtins = List.of("echo", "type", "exit");
         
         try (Scanner scanner = new Scanner(System.in)) {
             while(true) {
@@ -13,7 +16,18 @@ public class Main {
                 else if(input.split(" ")[0].equals("echo")) {
                     System.out.println(input.substring(input.indexOf(" ") + 1));
                 }
-                else System.out.println(input + ": command not found");
+                else if(input.split(" ")[0].equals("type")) {
+                    String command = input.split(" ")[1];
+                    if(builtins.contains(command)) {
+                        System.out.println(command + " is a shell builtin");
+                    }
+                    else {
+                        System.out.println(command + ": not found");
+                    }
+                }
+                else {
+                    System.out.println(input + ": command not found");
+                }
             }
         }
     }
